a8d822a89e89d97203dbbe7cb9aa75cd6fdb64e7
Tiago Albineli Motta <timotta@gmail.com>
2017-09-07 10:38:03 -0300

    doing user partial derivative, stoped due to breeze reshape bug

--- src/main/scala/com/timotta/rec/xclimf/XCLiMF.scala
+++ src/main/scala/com/timotta/rec/xclimf/XCLiMF.scala
@@ -93,27 +93,43 @@ class XCLiMF[T: ClassTag](
 
     val fmi_fmk = fmi.-:-(fmk)
     val fmk_fmi = fmk.-:-(fmi)
-
     val ymi = iteraction.itemRatings.toDenseMatrix
     val ymk = ymi.t
     val ymitile = tile(ymi, fmi_fmk.rows, 1)
     val ymktile = tile(ymk, 1, fmk_fmi.cols)
     val g_fmi = sigmoid(-1.0 * fmiv)
 
+    //items partial increments
     val div1 = 1.0/(1.0 - (ymktile.*:*(sigmoid(fmk_fmi))))
     val div2 = 1.0/(1.0 - (ymitile.*:*(sigmoid(fmi_fmk))))
     val bimul = ymktile.*:*(dg(fmi_fmk).*:*(div1 - div2))
     val brackets_i = g_fmi + sum(bimul(::, *))
-
     val ymibru = ymi.*:*(brackets_i).t.*(iteraction.userFactors.toDenseMatrix)
     val di = gamma * (ymibru - lambda * iteraction.itemFactors)
+
+    val top = ymktile.*:*(dg(fmk_fmi))
+    val bot = 1.0 - (ymktile.*:*(sigmoid(fmk_fmi)))
+
+    val N2 = N * N
+
+    println("viks", iteraction.itemFactors )
+    println("vikstile", tile(iteraction.itemFactors, 1, N))
+
+    println("viksre", tile(iteraction.itemFactors, 1, N).t.reshape(1, N2*dims) )
+    println("viksre", tile(iteraction.itemFactors, 1, N).t.reshape(1, N2*dims).reshape(N2, dims) )
+
+    val vis = tile(iteraction.itemFactors, 1, N).t.reshape(N2, dims)
+    val vks = tile(iteraction.itemFactors, N, 1)
+    val sub = vis.-(vks)
+
+    println("vis", vis)
+    println("vks", vks)
+    println("sub", sub)
 
     Iteractions.Iteraction[T](iteraction.userFactors, iteraction.itemNames, iteraction.itemRatings, di)
   }
 
-  private def dg(x:DenseMatrix[Double]) = {
+  private def dg(x:DenseMatrix[Double]): DenseMatrix[Double] = {
     exp(x)/pow((exp(x) + 1.0), 2)
   }
-//    """derivative of sigmoid function"""
-//    return np.exp(x)/(1+np.exp(x))**2
 }
--- src/test/scala/com/timotta/rec/xclimf/XCLiMFTest.scala
+++ src/test/scala/com/timotta/rec/xclimf/XCLiMFTest.scala
@@ -3,11 +3,14 @@ package com.timotta.rec.xclimf
 import org.junit.Test
 import breeze.linalg.DenseVector
 import breeze.linalg.DenseMatrix
+import breeze.linalg._
+import breeze.numerics._
+import org.junit.Assert
 
 class XCLiMFTest {
 
   @Test
-  def updateOneUser(): Unit = {
+  def updateOneUser1(): Unit = {
 
     val xclimf = new XCLiMF[String](dims = 4, lambda = 0.1, gamma = 0.1)
 
@@ -21,4 +24,19 @@ class XCLiMFTest {
 
   }
 
+  @Test
+  def updateOneUser2(): Unit = {
+
+    val xclimf = new XCLiMF[String](dims = 4, lambda = 0.1, gamma = 0.1)
+
+    xclimf.updateOneUser("u1", Iteractions.Iteraction(
+      userFactors = DenseVector(0.02, 0.01, 0.03, 0.04),
+      itemNames = List("i1", "i2", "i3"),
+      itemRatings = DenseVector(0.2f, 0.5f, 0.7f),
+      itemFactors = DenseMatrix(
+        DenseVector(0.01, 0.02, 0.03, 0.015),
+        DenseVector(0.05, 0.07, 0.11, 0.101),
+        DenseVector(0.02, 0.01, 0.01, 0.03))))
+
+  }
 }
