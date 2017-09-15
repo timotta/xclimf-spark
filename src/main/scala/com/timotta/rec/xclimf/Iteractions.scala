31e920129bae09cde7190a6cb2867499679a58c8
Tiago Albineli Motta <timotta@gmail.com>
2017-09-15 15:40:55 -0300

    small refactoring to split responsabilities

--- src/main/scala/com/timotta/rec/xclimf/Iteractions.scala
+++ src/main/scala/com/timotta/rec/xclimf/Iteractions.scala
@@ -3,6 +3,7 @@ package com.timotta.rec.xclimf
 import org.apache.spark.rdd.RDD
 import breeze.linalg.DenseMatrix
 import breeze.linalg.DenseVector
+import scala.reflect.ClassTag
 
 object Iteractions {
   case class Iteraction[T](
@@ -17,4 +18,26 @@ object Iteractions {
   }
 
   type Iteractions[T] = RDD[(T, Iteraction[T])]
+
+  def prepare[T: ClassTag](users: RDD[(T, Array[(T, Double)])],
+    userFactors: Factors.Factors[T],
+    itemFactors: Factors.Factors[T]): Iteractions.Iteractions[T] = {
+    val numPartitions = users.partitions.size
+    users.flatMap {
+      case (user, ratings) =>
+        ratings.map {
+          case (item, rating) =>
+            (item, (user, rating))
+        }
+    }.join(itemFactors).map {
+      case (item, ((user, rating), itemFactor)) =>
+        (user, (item, rating, itemFactor))
+    }.groupBy(_._1).join(userFactors).map {
+      case (user, (items, userFactors)) =>
+        val itemNames = items.map(_._2._1).toList
+        val itemRatings = DenseMatrix(items.map(_._2._2).toList: _*).t
+        val itemFactors = DenseMatrix(items.map(_._2._3.toArray).toArray: _*)
+        (user, Iteractions.Iteraction(userFactors, itemNames, itemRatings, itemFactors))
+    }.repartition(numPartitions)
+  }
 }
--- src/main/scala/com/timotta/rec/xclimf/XCLiMF.scala
+++ src/main/scala/com/timotta/rec/xclimf/XCLiMF.scala
@@ -45,7 +45,7 @@ class XCLiMF[T: ClassTag](
 
     breakable {
       for (i <- 1 to maxIters) {
-        val iteraction = prepareIteraction(users, userFactors, itemFactors)
+        val iteraction = Iteractions.prepare(users, userFactors, itemFactors)
 
         if (objective.update(iteraction, userFactors, itemFactors)) {
           log.info("doing iteraction=" + i + ": last objective=" + objective.lastObjective)
@@ -60,28 +60,6 @@ class XCLiMF[T: ClassTag](
     }
   }
 
-  private def prepareIteraction(users: RDD[(T, Array[(T, Double)])],
-    userFactors: Factors.Factors[T],
-    itemFactors: Factors.Factors[T]): Iteractions.Iteractions[T] = {
-    val numPartitions = users.partitions.size
-    users.flatMap {
-      case (user, ratings) =>
-        ratings.map {
-          case (item, rating) =>
-            (item, (user, rating))
-        }
-    }.join(itemFactors).map {
-      case (item, ((user, rating), itemFactor)) =>
-        (user, (item, rating, itemFactor))
-    }.groupBy(_._1).join(userFactors).map {
-      case (user, (items, userFactors)) =>
-        val itemNames = items.map(_._2._1).toList
-        val itemRatings = DenseMatrix(items.map(_._2._2).toList: _*).t
-        val itemFactors = DenseMatrix(items.map(_._2._3.toArray).toArray: _*)
-        (user, Iteractions.Iteraction(userFactors, itemNames, itemRatings, itemFactors))
-    }.repartition(numPartitions)
-  }
-
   private def update(iteractions: Iteractions.Iteractions[T]) = {
     iteractions.map {
       case (user, iteraction) =>
@@ -90,7 +68,7 @@ class XCLiMF[T: ClassTag](
     }
   }
 
-  protected[xclimf] def updateOneUser(user: T, iteraction: Iteractions.Iteraction[T]): Iteractions.Iteraction[T] = {
+  protected[xclimf] def updateOneUser(user: T, iteraction: Iteraction[T]): Iteractions.Iteraction[T] = {
     val N = iteraction.itemNames.size
 
     val fmiv = fmiVector(iteraction)
@@ -131,7 +109,7 @@ class XCLiMF[T: ClassTag](
     //user factor summed
     val userFactors = iteraction.userFactors + du2
 
-    Iteractions.Iteraction[T](userFactors, iteraction.itemNames, iteraction.itemRatings, di)
+    Iteraction[T](userFactors, iteraction.itemNames, iteraction.itemRatings, di)
   }
 
   private def fmiVector(iteraction: Iteraction[T]): DenseMatrix[Double] = {
